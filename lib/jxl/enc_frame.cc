9c1c61ec40713527f0f80cbe51175aefe624c18e
Jonathan Brown <jonathanbr30@gmail.com>
2026-07-26 23:24:21 +0100

    Fix check

--- lib/jxl/enc_frame.cc
+++ lib/jxl/enc_frame.cc
@@ -510,7 +510,7 @@ Status MakeFrameHeader(size_t xsize, size_t ysize,
 
   if (jpeg_data) {
     frame_header->UpdateFlag(false, FrameHeader::kUseDcFrame);
-    if (!frame_header.chroma_subsampling.Is444()) {
+    if (!frame_header->chroma_subsampling.Is444()) {
       frame_header->UpdateFlag(true, FrameHeader::kSkipAdaptiveDCSmoothing);
     }
   }

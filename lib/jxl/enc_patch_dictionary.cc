c6465ed564a7acb29f381c7791cca0f756f0d3b1
Jonathan Brown <jonathanbr30@gmail.com>
2026-09-11 01:10:23 +0100

    Update enc_patch_dictionary.cc

--- lib/jxl/enc_patch_dictionary.cc
+++ lib/jxl/enc_patch_dictionary.cc
@@ -856,8 +856,7 @@ Status RoundtripPatchFrame(Image3F* reference_frame,
   AuxOut patch_aux_out;
   JXL_RETURN_IF_ERROR(EncodeFrame(
       memory_manager, cparams, patch_frame_info, state->shared.metadata, ib,
-      cms, pool, special_frame.get(), aux_out ? &patch_aux_out : nullptr,
-      true));
+      cms, pool, special_frame.get(), aux_out ? &patch_aux_out : nullptr));
   if (aux_out) {
     for (const auto& l : patch_aux_out.layers) {
       aux_out->layer(LayerType::Dictionary).Assimilate(l);

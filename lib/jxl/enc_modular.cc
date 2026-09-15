c40c8036400f5de6822619588534f85f4ef684f3
Jonathan Brown <jonathanbr30@gmail.com>
2026-09-15 06:48:26 +0100

    v2

--- lib/jxl/enc_modular.cc
+++ lib/jxl/enc_modular.cc
@@ -443,6 +443,7 @@ Status try_palettes(Image& gi, int& max_bitdepth, int& maxval,
                     float channel_colors_percent,
                     jxl::ThreadPool* pool = nullptr) {
   float cost_before = 0.f;
+  float cost_before_palette = 0.f;
   size_t did_palette = 0;
   float nb_pixels = gi.channel[0].w * gi.channel[0].h;
   int nb_chans = gi.channel.size() - gi.nb_meta_channels;
@@ -453,51 +454,53 @@ Status try_palettes(Image& gi, int& max_bitdepth, int& maxval,
     // when not estimating, assume some arbitrary bpp
     if (cparams_.speed_tier <= SpeedTier::kSquirrel) {
       JXL_ASSIGN_OR_RETURN(cost_before, EstimateCost(gi));
-      if (nb_chans == 3 && gi.nb_meta_channels == 0 &&
-          cparams_.color_transform == ColorTransform::kNone &&
-          cparams_.colorspace < 0) {
-        int nb_rcts_to_try = 0;
-        switch (cparams_.speed_tier) {
-          case SpeedTier::kSquirrel:
-            nb_rcts_to_try = 7;
-            break;
-          case SpeedTier::kKitten:
-            nb_rcts_to_try = 9;
-            break;
-          default:
-            nb_rcts_to_try = 19;
-            break;
-        }
-        std::vector<Channel> orig;
-        orig.reserve(3);
-        for (size_t c = 0; c < 3; ++c) {
-          Channel& genuine = gi.channel[c];
-          JXL_ASSIGN_OR_RETURN(
-              Channel ch, Channel::Create(genuine.memory_manager(), genuine.w,
-                                          genuine.h, genuine.hshift,
-                                          genuine.vshift));
-          JXL_RETURN_IF_ERROR(CopyImageTo(genuine.plane, &ch.plane));
-          orig.emplace_back(std::move(ch));
-        }
-        std::array<const Channel*, 3> in = {&orig[0], &orig[1], &orig[2]};
-        std::array<Channel*, 3> out = {&gi.channel[0], &gi.channel[1],
-                                       &gi.channel[2]};
-        for (int rct_type : {0 * 7 + 6, 0 * 7 + 5, 1 * 7 + 3, 3 * 7 + 5,
-                             5 * 7 + 5, 1 * 7 + 5, 2 * 7 + 5, 1 * 7 + 1,
-                             0 * 7 + 4, 1 * 7 + 2, 2 * 7 + 1, 2 * 7 + 2,
-                             2 * 7 + 3, 4 * 7 + 4, 4 * 7 + 5, 0 * 7 + 2,
-                             0 * 7 + 1, 0 * 7 + 3}) {
-          if (--nb_rcts_to_try <= 0) break;
-          JXL_RETURN_IF_ERROR(FwdRct(in, out, rct_type, pool));
-          JXL_ASSIGN_OR_RETURN(float cost_rct, EstimateCost(gi));
-          cost_before = std::min(cost_before, cost_rct);
-        }
-        for (size_t c = 0; c < 3; ++c) {
-          gi.channel[c].plane.Swap(orig[c].plane);
-        }
-      }
     } else {
       cost_before = nb_pixels * arbitrary_bpp_estimate;
+        }
+    cost_before_palette = cost_before;
+    if (cparams_.speed_tier <= SpeedTier::kSquirrel && nb_chans == 3 &&
+        gi.nb_meta_channels == 0 &&
+        cparams_.color_transform == ColorTransform::kNone &&
+        cparams_.colorspace < 0) {
+      int nb_rcts_to_try = 0;
+      switch (cparams_.speed_tier) {
+        case SpeedTier::kSquirrel:
+          nb_rcts_to_try = 7;
+          break;
+        case SpeedTier::kKitten:
+          nb_rcts_to_try = 9;
+          break;
+        default:
+          nb_rcts_to_try = 19;
+          break;
+      }
+      std::vector<Channel> orig;
+      orig.reserve(3);
+      for (size_t c = 0; c < 3; ++c) {
+        Channel& genuine = gi.channel[c];
+        JXL_ASSIGN_OR_RETURN(
+            Channel ch, Channel::Create(genuine.memory_manager(), genuine.w,
+                                        genuine.h, genuine.hshift,
+                                        genuine.vshift));
+        JXL_RETURN_IF_ERROR(CopyImageTo(genuine.plane, &ch.plane));
+        orig.emplace_back(std::move(ch));
+      }
+      std::array<const Channel*, 3> in = {&orig[0], &orig[1], &orig[2]};
+      std::array<Channel*, 3> out = {&gi.channel[0], &gi.channel[1],
+                                     &gi.channel[2]};
+      for (int rct_type : {0 * 7 + 6, 0 * 7 + 5, 1 * 7 + 3, 3 * 7 + 5,
+                           5 * 7 + 5, 1 * 7 + 5, 2 * 7 + 5, 1 * 7 + 1,
+                           0 * 7 + 4, 1 * 7 + 2, 2 * 7 + 1, 2 * 7 + 2,
+                           2 * 7 + 3, 4 * 7 + 4, 4 * 7 + 5, 0 * 7 + 2,
+                           0 * 7 + 1, 0 * 7 + 3}) {
+        if (--nb_rcts_to_try <= 0) break;
+        JXL_RETURN_IF_ERROR(FwdRct(in, out, rct_type, pool));
+        JXL_ASSIGN_OR_RETURN(float cost_rct, EstimateCost(gi));
+        cost_before_palette = std::min(cost_before_palette, cost_rct);
+      }
+      for (size_t c = 0; c < 3; ++c) {
+        gi.channel[c].plane.Swap(orig[c].plane);
+      }
     }
     // all-channel palette (e.g. RGBA)
     if (nb_chans > 1) {
@@ -525,7 +528,7 @@ Status try_palettes(Image& gi, int& max_bitdepth, int& maxval,
       JXL_ASSIGN_OR_RETURN(
           did_palette,
           try_palette_transform(gi, maybe_palette, cparams_, weighted::Header(),
-                                cost_before, pool));
+                                cost_before_palette, pool));
     }
     // all-minus-one-channel palette (RGB with separate alpha, or CMY with
     // separate K)
@@ -544,7 +547,7 @@ Status try_palettes(Image& gi, int& max_bitdepth, int& maxval,
       JXL_ASSIGN_OR_RETURN(
           did_palette,
           try_palette_transform(gi, maybe_palette_3, cparams_,
-                                weighted::Header(), cost_before, pool));
+                                weighted::Header(), cost_before_palette, pool));
     }
   }
 

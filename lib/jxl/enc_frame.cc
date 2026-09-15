fd0b38818ee567b16ef1877852c9b2c1e3e7209f
Jonathan Brown <jonathanbr30@gmail.com>
2026-09-16 00:50:59 +0100

    Fix

--- lib/jxl/enc_frame.cc
+++ lib/jxl/enc_frame.cc
@@ -511,9 +511,9 @@ Status MakeFrameHeader(size_t xsize, size_t ysize,
   if (jpeg_data) {
     frame_header->UpdateFlag(false, FrameHeader::kUseDcFrame);
     // Skip smoothing if set to 0, default to 444 only and on when set to 1.
-    if (!cparams.force_lfs_jpeg_recompression == 0) {
+    if (cparams.force_lfs_jpeg_recompression != 0) {
       if ((cparams.force_lfs_jpeg_recompression == -1 &&
-        frame_header.chroma_subsampling.Is444()) ||
+        frame_header->chroma_subsampling.Is444()) ||
         cparams.force_lfs_jpeg_recompression == 1) {
         frame_header->UpdateFlag(true,
         FrameHeader::kSkipAdaptiveDCSmoothing);
